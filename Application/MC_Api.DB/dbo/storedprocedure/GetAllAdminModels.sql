591f3185810f6d51ac1fa1714cd45d0b2f2b22e5
Zero <kevinhumberto9@hotmail.com>
2020-03-20 20:37:55 -0600

    Add SP models into DB
    Add SP:
     * GetAllAdminModels.sql
     * GetIdAdminModels.sql

---
+++ Application/MC_Api.DB/dbo/storedprocedure/GetAllAdminModels.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetAllAdminModels]
+  @Type INT
+AS
+BEGIN
+  IF @Type = 0
+    BEGIN
+      SELECT * 
+	   FROM [MCRoles] 
+	   WHERE [Available] = 1;
+	END
+  ELSE IF @Type = 1
+    BEGIN
+      SELECT *
+       FROM [MCUsers]
+       WHERE [Available] = 1;
+    END
+END
---
+++ Application/MC_Api.DB/dbo/storedprocedure/GetIdAdminModels.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[GetIdAdminModels]
+	@Type INT,
+	@Id UNIQUEIDENTIFIER
+AS
+BEGIN
+  IF @Type = 0
+    BEGIN
+	  SELECT * 
+	   FROM [MCRoles]
+	   WHERE [Id] = @Id;
+	END
+  IF @Type = 1
+    BEGIN
+	  SELECT *
+	   FROM [MCUsers]
+	   WHERE [Id] = @Id;
+	END
+END
